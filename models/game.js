6615ca87628387645ce12f8182118aff985efdd3
anwar1434 <anwarobeid75@gmail.com>
2024-05-15 01:09:28 +0300

    Update game.js

--- models/game.js
+++ models/game.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const gamesScema = mongoose.Schema( {
     name: String,
     price: Number,
-    image: String
+    photo: String
 } );
 
 export const GamesInfo = mongoose.model( "GamesInfo", gamesScema );
