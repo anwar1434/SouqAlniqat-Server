b718f85723833f1b6b026567c4b60ba0dc97b6be
anwar1434 <anwarobeid75@gmail.com>
2024-05-16 12:35:30 +0300

    Update update.js

--- router/update.js
+++ router/update.js
@@ -7,7 +7,7 @@ router.post("/:id" , async  (request , response) => {
     const { name, points } = request.body
     const {id} = request.params
     try {
-        const updateStudent = StudentInfo.findByIdAndUpdate( id, {  name,  points } ).exec();
+        const updateStudent = await StudentInfo.findByIdAndUpdate( id, {  name,  points } ).exec();
         return response.status(200).json({message: "تم تحديث بيانات الطالب بنجاح"})
     }
     catch (error){response.json({message:error.message})}
