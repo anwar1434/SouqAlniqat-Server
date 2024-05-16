6ad9d1d5964fb4dc51a14ef3c41d25fbebdacfa1
anwar1434 <anwarobeid75@gmail.com>
2024-05-16 15:14:08 +0300

    a

--- router/add.js
+++ router/add.js
@@ -2,7 +2,7 @@ import express from "express"
 import { StudentInfo } from "../models/student.js"; 
 import XLSX from "xlsx"
 const router = express.Router()
-const workbook = XLSX.readFile( "../FileA.xlsx" );
+const workbook = XLSX.readFile( "./FileA.xlsx" );
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 const data = XLSX.utils.sheet_to_json( worksheet, { header: 1 } );
 
