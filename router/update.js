bdccba82a0c374f708ccff660aba1c81214f1453
anwar1434 <anwarobeid75@gmail.com>
2024-05-16 11:48:19 +0300

    Update update.js

--- router/update.js
+++ router/update.js
@@ -3,7 +3,8 @@ import { StudentInfo } from "../models/student.js";
 import { GamesInfo } from "../models/game.js"
 
 router.post("/:id" , async  (request , response) => {
-    const { name , points , secretcode } = request.body
+    const { name, points, secretcode } = request.body
+    const {id} = request.params
     try {
         const updateStudent = StudentInfo.findByIdAndUpdate( id, { name: name, points: points } );
         return response.status(200).json({message: "تم تحديث بيانات الطالب بنجاح"})
