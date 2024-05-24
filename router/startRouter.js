f353dde08c3e96675760dea5b413bc0076c0f8cd
anwar1434 <anwarobeid75@gmail.com>
2024-05-24 16:53:11 +0300

    Update startRouter.js

--- router/startRouter.js
+++ router/startRouter.js
@@ -10,7 +10,7 @@ router.post("/" , async (request , response ) => {
         if ( !result || result.length == 0 ) { return response.status(404).json( { message: "الرمز غير موجدود الرجاء التأكد من الرمز ثم اعادة المحاولة" } ) }
         return response.status( 200 ).json( { message: "true" , id: result._id } )
     }
-    catch ( error ) { response.status( 500 ).json( { message: "erorr" } )}
+    catch ( error ) { response.status( 500 ).json( { message: "الرمز غير موجدود الرجاء التأكد من الرمز ثم اعادة المحاولة"  } )}
 } )
 
 router.get("/:id" , async (request , response) => {
